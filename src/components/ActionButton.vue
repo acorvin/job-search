7dc3f0c2154d454c4fef5d3026c8b34aec11c617
Alex Corvin <acorvin@oppgen.com>
2022-04-22 18:12:56 -0600

    adding computed property to nav button

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,15 +1,34 @@
 <template>
-  <button
-    class="px-6 py-2 font-medium text-white border-0 rounded bg-brand-blue hover:shadow-blue"
-  >
-    Log In
+  <button :class="buttonClass">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: 'ActionButton',
+  props: ['text'],
+  data() {
+    return {
+      primary: true,
+    };
+  },
+  computed: {
+    buttonClass() {
+      return {
+        primary: this.primary,
+      };
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+
+.primary {
+  @apply text-white bg-brand-blue hover:shadow-blue;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -21,7 +21,12 @@
         </nav>
         <div class="flex items-center h-full ml-auto">
           <profile-image v-if="isLoggedIn" data-test="profile-image" />
-          <action-button v-else data-test="login-button" @click="loginUser" />
+          <action-button
+            v-else
+            text="props text"
+            data-test="login-button"
+            @click="loginUser"
+          />
         </div>
       </div>
     </div>
