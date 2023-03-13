3bc2053b4f129339eedc720c357948a1af0b9e12
Alex Corvin <corvindesigns@gmail.com>
2023-03-13 15:44:59 -0600

    installing pinia, adding first action for user login

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
@@ -9,4 +10,6 @@ import App from './App.vue'
 
 library.add(faSearch)
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
