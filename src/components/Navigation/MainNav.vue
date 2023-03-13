fb6de9aaccb2e2c1b41fa3977e3439f72feeaf26
Alex Corvin <corvindesigns@gmail.com>
2023-03-13 16:45:01 -0600

    adding helper functions in order to map store state and actions

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -28,6 +28,9 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'pinia'
+import { useUserStore } from '@/stores/user'
+
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import ProfileImage from '@/components/Navigation/ProfileImage.vue'
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
@@ -47,11 +50,11 @@ export default {
         { text: 'How We Hire', url: '/' },
         { text: 'Students', url: '/' },
         { text: 'Jobs', url: '/jobs/results' }
-      ],
-      isLoggedIn: false
+      ]
     }
   },
   computed: {
+    ...mapState(useUserStore, ['isLoggedIn']),
     headerHeightClass() {
       return {
         'h-16': !this.isLoggedIn,
@@ -60,9 +63,7 @@ export default {
     }
   },
   methods: {
-    loginUser() {
-      this.isLoggedIn = true
-    }
+    ...mapActions(useUserStore, ['loginUser'])
   }
 }
 </script>
--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,17 +1,22 @@
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { RouterLinkStub } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
 
 import MainNav from '@/components/Navigation/MainNav.vue'
+import { useUserStore } from '@/stores/user'
 
 // Disply company name
 describe('MainNav', () => {
   const renderMainNav = () => {
+    const pinia = createTestingPinia()
+
     const $route = {
       name: 'Home'
     }
     render(MainNav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route
         },
@@ -28,10 +33,13 @@ describe('MainNav', () => {
     const companyName = screen.getByText('CorvinCareers')
     expect(companyName).toBeInTheDocument()
   })
+
   // loginUser
   describe('when the user logs in', () => {
     it('displays user profile picture', async () => {
       renderMainNav()
+      const userStore = useUserStore()
+
       let profileImage = screen.queryByRole('img', {
         name: /user profile image/i
       })
@@ -40,6 +48,7 @@ describe('MainNav', () => {
       const loginButton = screen.getByRole('button', {
         name: /sign in/i
       })
+      userStore.isLoggedIn = true
       await userEvent.click(loginButton)
 
       profileImage = screen.getByRole('img', {
