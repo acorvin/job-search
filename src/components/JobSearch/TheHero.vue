f7493d1284fe6a926c50261bc6e85268fb5e2f1d
Alex Corvin <corvindesigns@gmail.com>
2023-03-15 16:36:55 -0600

    creating and rendering slot components

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex h-screen flex-col">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
 
@@ -16,20 +16,41 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template v-slot:default="{ img, title, description }">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="img" class="object-contain" />
+
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">{{ title }}</h3>
+
+            <p class="mt-3 text-sm">{{ description }}</p>
+          </div>
+
+          <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1"
+            >View Jobs
+          </router-link>
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from '@/components/JobSearch/TheHeadline.vue'
 import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue'
+import SpotLight from '@/components/JobSearch/SpotLight.vue'
 
 export default {
   name: 'TheHero',
   components: {
     TheHeadline,
-    JobSearchForm
+    JobSearchForm,
+    SpotLight
   }
 }
 </script>
-
-<style scoped></style>
