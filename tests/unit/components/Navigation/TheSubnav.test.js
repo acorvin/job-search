3e6b038f8886aea0965c2a69276b3b06de0a42a2
Alex Corvin <corvindesigns@gmail.com>
2023-04-02 16:53:50 -0600

    setting the accurate number of filtered jobs to display

--- tests/unit/components/Navigation/TheSubnav.test.js
+++ tests/unit/components/Navigation/TheSubnav.test.js
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/vue'
+import { createTestingPinia } from '@pinia/testing'
 
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
+import { useJobsStore } from '@/stores/jobs'
 
 describe('TheSubnav', () => {
   const renderTheSubnav = (routeName) => {
+    const pinia = createTestingPinia()
+    const jobsStore = useJobsStore()
+
     render(TheSubnav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route: { name: routeName }
         },
@@ -19,14 +25,17 @@ describe('TheSubnav', () => {
         }
       }
     })
+    return { jobsStore }
   }
   describe('when user is on jobs page', () => {
-    it('displays job count', () => {
+    it('displays job count', async () => {
       const routeName = 'JobResults'
 
-      renderTheSubnav(routeName)
+      const { jobsStore } = renderTheSubnav(routeName)
+      const numberOfJobs = 16
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({})
 
-      const jobCount = screen.getByText('1653')
+      const jobCount = await screen.findByText(numberOfJobs)
       expect(jobCount).toBeInTheDocument()
     })
   })
@@ -34,9 +43,13 @@ describe('TheSubnav', () => {
     it('does NOT display job count', () => {
       const routeName = 'Home'
 
+      const { jobsStore } = renderTheSubnav(routeName)
+      const numberOfJobs = 16
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({})
+
       renderTheSubnav(routeName)
 
-      const jobCount = screen.queryByText('1653')
+      const jobCount = screen.queryByText(numberOfJobs)
       expect(jobCount).not.toBeInTheDocument()
     })
   })
