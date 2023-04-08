92afa76b4c95954a19354cba855b2021d8eecb59
Alex Corvin <corvindesigns@gmail.com>
2023-04-08 17:55:23 -0600

    adding multiple filters for jobs

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -33,12 +33,7 @@
 import { mapActions, mapState } from 'pinia'
 import JobListing from '@/components/JobResults/JobListing.vue'
 
-import {
-  useJobsStore,
-  FETCH_JOBS,
-  FILTERED_JOBS_BY_ORGANIZATIONS,
-  FILTERED_JOBS_BY_JOB_TYPES
-} from '@/stores/jobs'
+import { useJobsStore, FETCH_JOBS, FILTERED_JOBS } from '@/stores/jobs'
 
 export default {
   name: 'JobListings',
@@ -55,17 +50,17 @@ export default {
       return previousPage >= firstPage ? previousPage : undefined
     },
     ...mapState(useJobsStore, {
-      FILTERED_JOBS_BY_JOB_TYPES,
+      FILTERED_JOBS,
       nextPage() {
         const nextPage = this.currentPage + 1
-        const maxPage = Math.ceil(this.FILTERED_JOBS_BY_JOB_TYPES.length / 10)
+        const maxPage = Math.ceil(this.FILTERED_JOBS.length / 10)
         return nextPage <= maxPage ? nextPage : undefined
       },
       displayedJobs() {
         const pageNumber = this.currentPage
         const firstJobIndex = (pageNumber - 1) * 10
         const lastJobIndex = pageNumber * 10
-        return this.FILTERED_JOBS_BY_JOB_TYPES.slice(firstJobIndex, lastJobIndex)
+        return this.FILTERED_JOBS.slice(firstJobIndex, lastJobIndex)
       }
     })
   },
--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -7,6 +7,7 @@ import { useUserStore } from '@/stores/user'
 export const FETCH_JOBS = 'FETCH_JOBS'
 export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
 export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES'
+export const FILTERED_JOBS = 'FILTERED_JOBS'
 export const FILTERED_JOBS_BY_ORGANIZATIONS = 'FILTERED_JOBS_BY_ORGANIZATIONS'
 export const FILTERED_JOBS_BY_JOB_TYPES = 'FILTERED_JOBS_BY_JOB_TYPES'
 
@@ -46,6 +47,24 @@ export const useJobsStore = defineStore('jobs', {
         return state.jobs
       }
       return state.jobs.filter((job) => userStore.selectedJobTypes.includes(job.jobType))
+    },
+    [FILTERED_JOBS](state) {
+      const userStore = useUserStore()
+
+      const noSelectedOrganizations = userStore.selectedOrganizations.length === 0
+      const noSelectedJobTypes = userStore.selectedJobTypes.length === 0
+
+      return state.jobs
+        .filter((job) => {
+          if (noSelectedOrganizations) return true
+
+          return userStore.selectedOrganizations.includes(job.organization)
+        })
+        .filter((job) => {
+          if (noSelectedJobTypes) return true
+
+          return userStore.selectedJobTypes.includes(job.jobType)
+        })
     }
   }
 })
