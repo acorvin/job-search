6f8944c542308ed205d5ac4a4444578c63ea81d9
Alex Corvin <corvindesigns@gmail.com>
2023-04-08 18:11:41 -0600

    adding getters with arguments for filtered jobs

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -11,6 +11,9 @@ export const FILTERED_JOBS = 'FILTERED_JOBS'
 export const FILTERED_JOBS_BY_ORGANIZATIONS = 'FILTERED_JOBS_BY_ORGANIZATIONS'
 export const FILTERED_JOBS_BY_JOB_TYPES = 'FILTERED_JOBS_BY_JOB_TYPES'
 
+export const INCLUDE_JOB_BY_ORGANIZATION = 'INCLUDE_JOB_BY_ORGANIZATION'
+export const INCLUDE_JOB_BY_JOB_TYPE = 'INCLUDE_JOB_BY_JOB_TYPE'
+
 export const useJobsStore = defineStore('jobs', {
   state: () => ({
     jobs: []
@@ -32,6 +35,18 @@ export const useJobsStore = defineStore('jobs', {
       state.jobs.forEach((job) => uniqueJobTypes.add(job.jobType))
       return uniqueJobTypes
     },
+    [INCLUDE_JOB_BY_ORGANIZATION]: () => (job) => {
+      const userStore = useUserStore()
+
+      if (userStore.selectedOrganizations.length === 0) return true
+      return userStore.selectedOrganizations.includes(job.organization)
+    },
+    [INCLUDE_JOB_BY_JOB_TYPE]: () => (job) => {
+      const userStore = useUserStore()
+
+      if (userStore.selectedJobTypes.length === 0) return true
+      return userStore.selectedJobTypes.includes(job.jobType)
+    },
     [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
       const userStore = useUserStore()
 
@@ -49,22 +64,9 @@ export const useJobsStore = defineStore('jobs', {
       return state.jobs.filter((job) => userStore.selectedJobTypes.includes(job.jobType))
     },
     [FILTERED_JOBS](state) {
-      const userStore = useUserStore()
-
-      const noSelectedOrganizations = userStore.selectedOrganizations.length === 0
-      const noSelectedJobTypes = userStore.selectedJobTypes.length === 0
-
       return state.jobs
-        .filter((job) => {
-          if (noSelectedOrganizations) return true
-
-          return userStore.selectedOrganizations.includes(job.organization)
-        })
-        .filter((job) => {
-          if (noSelectedJobTypes) return true
-
-          return userStore.selectedJobTypes.includes(job.jobType)
-        })
+        .filter((job) => this.INCLUDE_JOB_BY_ORGANIZATION(job))
+        .filter((job) => this.INCLUDE_JOB_BY_JOB_TYPE(job))
     }
   }
 })
