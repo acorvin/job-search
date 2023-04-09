5f4c454721cceac8bec656265e248427a124cdf6
Alex Corvin <corvindesigns@gmail.com>
2023-04-09 08:31:47 -0600

    updating the components setup syntax

--- src/components/Shared/ActionButton.vue
+++ src/components/Shared/ActionButton.vue
@@ -4,35 +4,31 @@
   </button>
 </template>
 
-<script>
+<script setup>
 import { computed, toRefs } from 'vue'
-export default {
-  name: 'ActionButton',
-  props: {
-    text: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'primary',
-      validator(value) {
-        return ['primary', 'secondary'].includes(value)
-      }
-    }
-  },
-  setup(props) {
-    const { type } = toRefs(props)
 
-    const buttonClass = computed(() => {
-      return {
-        [type.value]: true
-      }
-    })
-    return { buttonClass }
+const props = defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: false,
+    default: 'primary',
+    validator(value) {
+      return ['primary', 'secondary'].includes(value)
+    }
   }
-}
+})
+
+const { type } = toRefs(props)
+
+const buttonClass = computed(() => {
+  return {
+    [type.value]: true
+  }
+})
 </script>
 
 <style scoped>
--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -16,30 +16,20 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { computed, ref } from 'vue'
 
-export default {
-  name: 'CollapsibleAccordion',
-  props: {
-    header: {
-      type: String,
-      required: true
-    }
-  },
-  setup() {
-    const isOpen = ref(false)
-
-    const open = () => {
-      isOpen.value = !isOpen.value
-    }
-    const caretIcon = computed(() => (isOpen.value ? ['fas', 'angle-up'] : ['fas', 'angle-down']))
-
-    return {
-      caretIcon,
-      isOpen,
-      open
-    }
+defineProps({
+  header: {
+    type: String,
+    required: true
   }
+})
+
+const isOpen = ref(false)
+
+const open = () => {
+  isOpen.value = !isOpen.value
 }
+const caretIcon = computed(() => (isOpen.value ? ['fas', 'angle-up'] : ['fas', 'angle-down']))
 </script>
