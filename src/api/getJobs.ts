4d9c145ae2a42e6d97fadf198135488286c4a87e
Alex Corvin <corvindesigns@gmail.com>
2023-04-09 18:44:59 -0600

    create jobs Interface and update api call

--- src/api/getJobs.ts
+++ src/api/getJobs.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+import type { Job } from '@/api/types'
+
 const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL
   const url = `${baseUrl}/jobs`
 
-  const response = await axios.get(url)
+  const response = await axios.get<Job[]>(url)
 
   return response.data
 }
