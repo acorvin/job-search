cfe6aee97ca0f2d6453687e4cbe6757a8e42a799
Alex Corvin <corvindesigns@gmail.com>
2023-04-10 05:45:51 -0600

    converting pinia stores to typescript

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,8 +3,14 @@ import { defineStore } from 'pinia'
 export const ADD_SELECTED_ORGANIZATIONS = 'ADD_SELECTED_ORGANIZATIONS'
 export const ADD_SELECTED_JOB_TYPES = 'ADD_SELECTED_JOB_TYPES'
 
+export interface UserState {
+  isLoggedIn: boolean
+  selectedOrganizations: string[]
+  selectedJobTypes: string[]
+}
+
 export const useUserStore = defineStore('user', {
-  state: () => ({
+  state: (): UserState => ({
     isLoggedIn: false,
     selectedOrganizations: [],
     selectedJobTypes: []
@@ -13,10 +19,10 @@ export const useUserStore = defineStore('user', {
     loginUser() {
       this.isLoggedIn = true
     },
-    [ADD_SELECTED_ORGANIZATIONS](organizations) {
+    [ADD_SELECTED_ORGANIZATIONS](organizations: string[]) {
       this.selectedOrganizations = organizations
     },
-    [ADD_SELECTED_JOB_TYPES](jobTypes) {
+    [ADD_SELECTED_JOB_TYPES](jobTypes: string[]) {
       this.selectedJobTypes = jobTypes
     }
   }
