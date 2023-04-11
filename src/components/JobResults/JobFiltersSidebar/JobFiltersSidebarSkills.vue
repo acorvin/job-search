097ebdc87bc8936d74532984d256434967fd67bd
Alex Corvin <corvindesigns@gmail.com>
2023-04-11 17:27:11 -0600

    adding modifiers to search input

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-2">
     <input
-      v-model.lazy="skillsSearchTerm"
+      v-model.lazy.trim="skillsSearchTerm"
       type="text"
       class="mt-2 w-full rounded border border-solid border-brand-gray-1 p-3 text-base shadow-gray"
       placeholder="Computer programming, Finance degree"
