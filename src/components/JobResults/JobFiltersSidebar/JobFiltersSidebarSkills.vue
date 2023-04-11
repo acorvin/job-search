a3b93c8663ba8ae71af2c8b945c06549030de7b6
Alex Corvin <corvindesigns@gmail.com>
2023-04-11 17:25:01 -0600

    adding modifiers to search term

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-2">
     <input
-      v-model="skillsSearchTerm"
+      v-model.lazy="skillsSearchTerm"
       type="text"
       class="mt-2 w-full rounded border border-solid border-brand-gray-1 p-3 text-base shadow-gray"
       placeholder="Computer programming, Finance degree"
